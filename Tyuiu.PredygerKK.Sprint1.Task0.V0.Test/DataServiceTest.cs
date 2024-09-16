e1018116f7febaa2ad2bd1a9730118412ebf9aa2
Tyuiu.PredygerKK@outlook.com <Tyuiu.PredygerKK@outlook.com>
2024-09-16 22:58:47 +0500

    Add project files.

---
+++ Tyuiu.PredygerKK.Sprint1.Task0.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.PredygerKK.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.PredygerKK.Sprint1.Task0.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
