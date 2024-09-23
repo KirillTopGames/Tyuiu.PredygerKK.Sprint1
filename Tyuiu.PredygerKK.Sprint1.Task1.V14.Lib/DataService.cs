4e9255cd24dcde8f2648bd1c066e1a43942353dc
Tyuiu.PredygerKK@outlook.com <Tyuiu.PredygerKK@outlook.com>
2024-09-23 17:09:41 +0500

    Task1

--- Tyuiu.PredygerKK.Sprint1.Task1.V14.Lib/DataService.cs
+++ Tyuiu.PredygerKK.Sprint1.Task1.V14.Lib/DataService.cs
@@ -1,4 +1,6 @@
-﻿namespace Tyuiu.PredygerKK.Sprint1.Task1.V14.Lib
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.PredygerKK.Sprint1.Task1.V14.Lib
 {
     public class DataService
     {
